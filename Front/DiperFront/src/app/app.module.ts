d1a62eeb375afcc82d2902088d7cb64b039d60c2
MaciasJH <macias.joelh@gmail.com>
2018-08-24 05:15:33 -0500

    Agregados los campos de cliente .1

--- Front/DiperFront/src/app/app.module.ts
+++ Front/DiperFront/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule, MatGridListModule, MatSelectModule} from '@angular/material';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
@@ -16,7 +16,7 @@ import { LayoutModule } from '@angular/cdk/layout';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule],
+    MatButtonModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatInputModule, MatGridListModule, MatSelectModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Front/DiperFront/src/app/form/form.component.ts
+++ Front/DiperFront/src/app/form/form.component.ts
@@ -9,6 +9,7 @@ import { formatDate } from '../../../node_modules/@angular/common';
 export class FormComponent implements OnInit {
   fecha = new Date();
   jsfecha = '';
+  selected = 'option2';
   constructor() { 
     this.jsfecha = formatDate(this.fecha, 'dd/MM/yyyy', 'en-US', '-0500');
   }
